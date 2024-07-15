e419b05298c40832e1f83d932bf9d01910795b01
Monika Luu <monika-luu@hotmail.com>
2024-07-15 11:46:23 +0200

    Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import React from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: { default: 'Monika Luu Portfolio', template: '%s | Portfolio' },
+  description:
+    'The official personal portfolio of Monika Luu, built with App Router of Next.js.',
+  metadataBase: new URL('https://portfolio-cyan-seven-70.vercel.app/'),
+};
 
 export default function RootLayout({
   children,
